2e3ee22d32ed8c3ceca5c8fb491db3afd78285f4
MaiteMB <122162891+MaiteMB@users.noreply.github.com>
2026-05-20 10:42:07 -0300

    Agregar manejo de sesion con Singleton

---
+++ Servicios_Salem/SesionManager.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BE_Salem;
+
+namespace Servicios_Salem
+{
+    public class SesionManager
+    {
+        private static SesionManager _instancia;
+
+        public Usuario_BE UsuarioActual { get; private set; }
+        private SesionManager()
+        {
+        }
+
+        public static SesionManager Instancia
+        {
+            get
+            {
+                if (_instancia == null)
+                {
+                    _instancia = new SesionManager();
+                }
+
+                return _instancia;
+            }
+        }
+
+        public void IniciarSesion(Usuario_BE usuario)
+        {
+            UsuarioActual = usuario;
+        }
+
+        public void CerrarSesion()
+        {
+            UsuarioActual = null;
+        }
+        public bool HaySesionActiva()
+        {
+            return UsuarioActual != null;
+        }
+    }
+}
